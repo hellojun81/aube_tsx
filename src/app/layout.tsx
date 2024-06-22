31f8e91ad5d0e6c58cbc00e4a2107cf627807689
김완준 <>
2024-06-22 09:55:22 +0900

    로컬 변경 사항 커밋

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,12 +28,8 @@ export default function RootLayout({ children, }: Readonly<{
     <html lang="en">
     <Head>
     <title>aubestudio</title>
-        <meta property="og:title" content="aubestudio" />
-        <meta property="og:description" content="everything is possible." />
         <meta property="og:image" content="/preview.jpg" />
-        <meta property="og:url" content="https://aubestudio.com" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
-   
+        <meta property="og:url" content="https://aubestudio.com" />  
       </Head>
       <body>
       <Menu />
