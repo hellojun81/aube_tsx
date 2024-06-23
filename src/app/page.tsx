4fa1ba2571757fd55ee97bcea78733efa4bdeaf5
김채현 <kimchaehyun@gimchaehyeon-ui-MacBookPro.local>
2024-06-23 14:15:53 +0900

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   useEffect(() => {
     const timer = setTimeout(() => {
       router.push('/main'); // 이동할 페이지 경로를 지정합니다.
-    }, 3000); // 3000ms = 3초
+    }, 2000); // 3000ms = 3초
     return () => clearTimeout(timer); // 컴포넌트 언마운트 시 타이머를 정리합니다.
   }, [router]);
 
