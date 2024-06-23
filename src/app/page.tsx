93afb9d662f20d3d6c93848c96dfba88b63387b5
김완준 <>
2024-06-23 18:03:58 +0900

    load img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,19 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
+
+    const images = [
+      '/images/1.jpg',
+      '/images/2.jpg',
+      '/images/3.jpg',
+  ];
+  images.forEach((src) => {
+    const img = new Image();
+    img.src = src;
+});
     const timer = setTimeout(() => {
       router.push('/main'); // 이동할 페이지 경로를 지정합니다.
-    }, 2000); // 3000ms = 3초
+    }, 2000); // 2000ms = 2초
     return () => clearTimeout(timer); // 컴포넌트 언마운트 시 타이머를 정리합니다.
   }, [router]);
 
