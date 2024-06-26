7945cb3ee8b71e3c6ead900dc9248378ea35f100
김완준 <>
2024-06-26 21:00:15 +0900

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// ES 모듈 형식의 임포트
+import path from 'path';
 
-export default nextConfig;
+// 설정 객체를 내보냅니다.
+export default {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false
+      };
+    }
+
+    // 서버 측 코드에서 파일 접근 경로 설정
+    config.resolve.alias['@public'] = path.join(process.cwd(), 'public');
+
+    return config;
+  },
+};
