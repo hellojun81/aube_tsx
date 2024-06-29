91433a82234069643fd7d92fd16f4d66cdf4fe6a
김완준 <>
2024-06-29 16:21:23 +0900

    i

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,26 +9,26 @@ export default function Home() {
 
 
   useEffect(() => {
-    const windowHeight = window.innerHeight;        // 윈도우의 높이와 너비를 가져옵니다.
-    const windowWidth = window.innerWidth;          // div 요소의 높이와 너비를 가져옵니다.
-    if (windowHeight < windowWidth) {
-      setimagesPath('/images/jpg_width')
-    }
+    // const windowHeight = window.innerHeight;        // 윈도우의 높이와 너비를 가져옵니다.
+    // const windowWidth = window.innerWidth;          // div 요소의 높이와 너비를 가져옵니다.
+    // if (windowHeight < windowWidth) {
+    //   setimagesPath('/images/jpg_width')
+    // }
 
-    const preloadImages = (srcs: string[]) => {
-      srcs.forEach((src) => {
-        const img = new Image();
-        img.src = src;
-      });
-    };
+    // const preloadImages = (srcs: string[]) => {
+    //   srcs.forEach((src) => {
+    //     const img = new Image();
+    //     img.src = src;
+    //   });
+    // };
 
 
-    const images = [
-      imagesPath+'/1.jpg',
-      imagesPath+'/2.jpg',
-      imagesPath+'/3.jpg',
-    ];
-    preloadImages(images);
+    // const images = [
+    //   imagesPath+'/1.jpg',
+    //   imagesPath+'/2.jpg',
+    //   imagesPath+'/3.jpg',
+    // ];
+    // preloadImages(images);
     
     const timer = setTimeout(() => {
       router.push('/main'); // 이동할 페이지 경로를 지정합니다.
