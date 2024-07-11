15db443108a8ab1e891930c150baff007eba028b
김완준 <>
2024-07-12 05:55:24 +0900

    init

---
+++ src/pages/ImageGallery.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ImageGallery from 'react-image-gallery';
+import "react-image-gallery/styles/css/image-gallery.css";
+
+interface ImageGalleryProps {
+  images: Array<{ original: string;}>;
+}
+
+const CustomImageGallery: React.FC<ImageGalleryProps> = ({ images }) => {
+  return <ImageGallery items={images} showFullscreenButton={true} 
+  showThumbnails={false}
+  // showNav={false}
+  disableSwipe={true}
+  />;
+};
+
+export default CustomImageGallery;
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,39 +1,33 @@
-import { motion, useAnimation, useViewportScroll } from 'framer-motion';
+import React from 'react';
+import CustomImageGallery from './ImageGallery';
+const screenMode='width'
+const images = [
+  {
+    original: `./test/1.jpg`,
+  },
+  {
+    original: `./test/2.jpg`,
+  },
+  {
+    original: `./test/3.jpg`,
+  },
+  {
+    original: `./test/4.jpg`,
+  },
+  {
+    original: `./test/5.jpg`,
+  },
 
-const App = () => {
-  const { scrollY } = useViewportScroll();
-  const controls = useAnimation();
+  // 더 많은 이미지를 추가할 수 있습니다.
+];
 
-  // scrollY 값에 따라 애니메이션 효과 제어
-  scrollY.onChange(y => {
-    if (y >= 100) { // 스크롤이 100px 이상일 때
-      controls.start({ opacity: 1, y: 0 });
-    } else { // 스크롤이 100px 미만일 때
-      controls.start({ opacity: 0, y: -100 });
-    }
-  });
-
+const GalleryPage: React.FC = () => {
   return (
     <div>
-      <motion.section
-        // initial={{ opacity: 0, y: -100 }}
-        // animate={controls}
-        transition={{ duration: 0.5 }}
-        style={{backgroundColor: 'lightgreen' , height: '100vh',position: 'fixed', width: '100%',  zIndex: -1 ,}}
-      >
-        <h2>Fixed Section</h2>
-      </motion.section>
-
-      <section style={{ height: '100vh', backgroundColor: 'red', marginTop: '100px' , zIndex: 2 }}>
-        <h2>Content Below Fixed Section</h2>
-      </section>
-
-      {/* 다른 섹션들 */}
-      <section style={{ height: '100vh', backgroundColor: 'blue' }}>
-        <h2>More Content</h2>
-      </section>
+      <h1>Image Gallery</h1>
+      <CustomImageGallery images={images} />
     </div>
   );
 };
 
-export default App;
+export default GalleryPage;
