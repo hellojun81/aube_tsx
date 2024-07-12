19e36032c5f8e6ce7e2d9c6dbc1472184d661d51
Kim wanjun <hellojun82@gmaill.com>
2024-07-12 16:01:36 +0900

    t

--- src/pages/ImageGallery.tsx
+++ src/pages/ImageGallery.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import ImageGallery from 'react-image-gallery';
-import "react-image-gallery/styles/css/image-gallery.css";
+// import React from 'react';
+// import ImageGallery from 'react-image-gallery';
+// import "react-image-gallery/styles/css/image-gallery.css";
 
-interface ImageGalleryProps {
-  images: Array<{ original: string;}>;
-}
+// interface ImageGalleryProps {
+//   images: Array<{ original: string;}>;
+// }
 
-const CustomImageGallery: React.FC<ImageGalleryProps> = ({ images }) => {
-  return <ImageGallery items={images} showFullscreenButton={true} 
-  showThumbnails={false}
-  // showNav={false}
-  disableSwipe={true}
-  />;
-};
+// const CustomImageGallery: React.FC<ImageGalleryProps> = ({ images }) => {
+//   return <ImageGallery items={images} showFullscreenButton={true} 
+//   showThumbnails={false}
+//   // showNav={false}
+//   disableSwipe={true}
+//   />;
+// };
 
-export default CustomImageGallery;
+// export default CustomImageGallery;
