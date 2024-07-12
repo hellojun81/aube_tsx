8813e52c0c8afcf83f04527b8d06323d4efdb2c2
Kim wanjun <hellojun82@gmaill.com>
2024-07-12 15:58:17 +0900

    t

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,33 +1,33 @@
-import React from 'react';
-import CustomImageGallery from './ImageGallery';
-const screenMode='width'
-const images = [
-  {
-    original: `./test/1.jpg`,
-  },
-  {
-    original: `./test/2.jpg`,
-  },
-  {
-    original: `./test/3.jpg`,
-  },
-  {
-    original: `./test/4.jpg`,
-  },
-  {
-    original: `./test/5.jpg`,
-  },
+// import React from 'react';
+// import CustomImageGallery from './ImageGallery';
+// const screenMode='width'
+// const images = [
+//   {
+//     original: `./test/1.jpg`,
+//   },
+//   {
+//     original: `./test/2.jpg`,
+//   },
+//   {
+//     original: `./test/3.jpg`,
+//   },
+//   {
+//     original: `./test/4.jpg`,
+//   },
+//   {
+//     original: `./test/5.jpg`,
+//   },
 
-  // 더 많은 이미지를 추가할 수 있습니다.
-];
+//   // 더 많은 이미지를 추가할 수 있습니다.
+// ];
 
-const GalleryPage: React.FC = () => {
-  return (
-    <div>
-      <h1>Image Gallery</h1>
-      <CustomImageGallery images={images} />
-    </div>
-  );
-};
+// const GalleryPage: React.FC = () => {
+//   return (
+//     <div>
+//       <h1>Image Gallery</h1>
+//       <CustomImageGallery images={images} />
+//     </div>
+//   );
+// };
 
-export default GalleryPage;
+// export default GalleryPage;
