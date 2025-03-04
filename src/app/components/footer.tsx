d31a806f3881173b823f0312e0fff903eb529b4b
jun <jun@junui-MacBookAir.local>
2025-03-04 14:19:00 +0900

    t

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -111,6 +111,40 @@ const Home = () => {
         }}
       />
 
+{/* /*linked : Start  */}
+     {/* LinkedIn Tracking */}
+     <Script id="linkedin-tracking" strategy="afterInteractive">
+        {`
+          window._linkedin_partner_id = "7007428";
+          window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+          window._linkedin_data_partner_ids.push(window._linkedin_partner_id);
+
+          (function(l) {
+            if (!l){
+              window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+              window.lintrk.q=[];
+            }
+            var s = document.getElementsByTagName("script")[0];
+            var b = document.createElement("script");
+            b.type = "text/javascript"; b.async = true;
+            b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+            s.parentNode.insertBefore(b, s);
+          })(window.lintrk);
+        `}
+      </Script>
+
+      <noscript>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: `
+              <img height="1" width="1" style="display:none;" alt=""
+              src="https://px.ads.linkedin.com/collect/?pid=7007428&fmt=gif" />
+            `,
+          }}
+        />
+      </noscript>
+
+{/* /*linked : End  */}
 
       {/* statcounter:start */}
       <Script >
